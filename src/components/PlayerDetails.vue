d713b1bd43122b3bcc65759324af9ae7f53ab134
pabloghid <pablodg290@gmail.com>
2023-12-04 02:21:35 -0300

    feat: more details player list

--- src/components/PlayerDetails.vue
+++ src/components/PlayerDetails.vue
@@ -9,6 +9,8 @@
                         <p class="card-text">{{ playerDetails.Position }}</p>
                         <p class="card-text">Idade: {{ playerDetails.Age }} anos</p>
                         <p class="card-text">País: {{ playerDetails.Nationality }}</p>
+                        <p class="card-text">Peso: {{ playerDetails.Weight_kg }} kg</p>
+                        <p class="card-text">Altura: {{ playerDetails.Height_cm }} m</p>
                         <p class="card-text"><small class="text-muted">Liga: {{ playerDetails.player_league_name }}</small>
                         </p>
                     </div>
@@ -20,87 +22,109 @@
                     Jogador na lista
                 </button>
             </div>
+
             <div class="col-md-8">
                 <div v-if="playerDetails" class="card">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="card-body">
-                                <img :src="playerDetails.player_team_logo" class="img-fluid" alt="Escudo do Time">
-                                <p class="card-text mt-2">Equipe atual: {{ playerDetails.Team }}</p>
+                                <img :src="playerDetails.player_team_logo" class="img-fluid mx-auto" alt="Escudo do Time">
+                                <p class="card-text mt-2 text-center">Equipe atual: {{ playerDetails.Team }}</p>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="card-body">
-                                <h5 class="card-title">Estatísticas Gerais</h5>
-                                <p class="card-text">Jogos: {{ playerDetails.Games }}</p>
-                                <p class="card-text">Minutos jogados: {{ playerDetails.Minutes }}</p>
 
-
+                        <div class="col-md-6">
+                            <div class="card border-0">
+                                <div class="card-body">
+                                    <h5 class="card-title">Estatísticas Gerais</h5>
+                                    <p class="card-text">Jogos: {{ playerDetails.Games }}</p>
+                                    <p class="card-text">Minutos jogados: {{ playerDetails.Minutes }}</p>
+                                    <p class="card-text">Jogos com 90 minutos: {{ playerDetails.Ninety_S }}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="card-body">
-                                <h5 class="card-title">Estatísticas de ataque</h5>
-                                <p class="card-text">Gols na liga: {{ playerDetails.Goals_Total }}</p>
-                                <p class="card-text">Gols/jogo: {{ playerDetails.Goals }}</p>
-                                <p class="card-text">Chutes: {{ playerDetails.Shots_Total }}</p>
-                                <p class="card-text">Chutes ao gol: {{ playerDetails.Shots_On }}</p>
-                                <p class="card-text">Chutes/jogo: {{ playerDetails.Shots }}</p>
-                                <p class="card-text">Chutes ao gol/jogo: {{ playerDetails.ShotsOnTarget }}</p>
-                                <p class="card-text">Porcentagem de chutes ao gol: {{ playerDetails.ShotsOnTarget_Percentage
-                                }}%</p>
-                                <p class="card-text">Gols/chute: {{ playerDetails.Goals_Shot }}</p>
-                                <p class="card-text">Gols/chutes ao gol: {{ playerDetails.Goals_ShotsOnTarget }}</p>
-
+                        
+                        <div v-if="playerDetails.Save_Percentage > 0" class="col-md-6">
+                            <div class="card border-0">
+                                <div class="card-body">
+                                    <h5 class="card-title">Estatísticas de goleiro</h5>
+                                    <p class="card-text">Gols concedidos: {{ playerDetails.Goals_Conceded }}</p>
+                                    <p class="card-text">Gols concedidos/jogo: {{ playerDetails.Goals_Conceded_n }}</p>
+                                    <p class="card-text">Defesas: {{ playerDetails.Goals_Saves }}</p>
+                                    <p class="card-text">Porcentagem de defesas: {{ playerDetails.Save_Percentage
+                                    }}%</p>
+                                </div>
                             </div>
                         </div>
+                        
                         <div class="col-md-6">
-                            <div class="card-body">
-                                <h5 class="card-title">Estatisticas de Defesa</h5>
-                                <p class="card-text">Bloqueios: {{ playerDetails.Tackled_Block }}</p>
-                                <p class="card-text">Bloqueios/jogo: {{ playerDetails.Blocks }}</p>
-                                <p class="card-text">Interceptações: {{ playerDetails.Tackled_Intercept }}</p>
-                                <p class="card-text">Interceptações/jogo: {{ playerDetails.Interceptations }}</p>
-                                <p class="card-text">Desarmes: {{ playerDetails.Tackled_Total }}</p>
-                                <p class="card-text">Desarmes/jogo: {{ playerDetails.Tackles }}</p>
-                                <p class="card-text">Desarmes e interceptações/jogo: {{
-                                    playerDetails.Tackles_Interceptations }}</p>
-                                <p class="card-text">Duelos ganhos: {{ playerDetails.Duels_Won }}</p>
-                                <p class="card-text">Total de duelos: {{ playerDetails.Duels_Total }}</p>
-                                <p class="card-text">Porcentagem de duelos ganhos: {{ playerDetails.Duels_Won_Percentage }}%
-                                </p>
-                                <p class="card-text">Faltas cometidas: {{ playerDetails.Fouls_Committed }}</p>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="card-body">
-                                <h5 class="card-title">Estatísticas de criação</h5>
-                                <p class="card-text">Assistências na liga: {{ playerDetails.Goals_Assist }}</p>
-                                <p class="card-text">Assistências/Jogo: {{ playerDetails.Assists }}</p>
-                                <p class="card-text">Precisão dos passes: {{ playerDetails.Accuracy_Passes }}%</p>
-                                <p class="card-text">Passes chave: {{ playerDetails.Key_Passes }}</p>
-                                <p class="card-text">Passes chave/jogo: {{ playerDetails.Key_Passes_n }}</p>
-                                <p class="card-text">Total de passes: {{ playerDetails.Total_Passes }}</p>
-                                <p class="card-text">Passes/jogo: {{ playerDetails.Passes_Total_n }}</p>
-                                <p class="card-text">Dribles tentados: {{ playerDetails.Dribbles_Attempts }}</p>
-                                <p class="card-text">Dribles completos: {{ playerDetails.Dribbles_Success }}</p>
-                                <p class="card-text">Porcentagem de dribles completos: {{
-                                    playerDetails.Dribbles_Success_Percentage }}%</p>
-                                <p class="card-text">Faltas recebidas: {{ playerDetails.Fouls_Drawn }}</p>
+                            <div class="card border-0">
+                                <div class="card-body">
+                                    <h5 class="card-title">Estatísticas de ataque</h5>
+                                    <p class="card-text">Gols na liga: {{ playerDetails.Goals_Total }}</p>
+                                    <p class="card-text">Gols/jogo: {{ playerDetails.Goals }}</p>
+                                    <p class="card-text">Chutes: {{ playerDetails.Shots_Total }}</p>
+                                    <p class="card-text">Chutes ao gol: {{ playerDetails.Shots_On }}</p>
+                                    <p class="card-text">Chutes/jogo: {{ playerDetails.Shots }}</p>
+                                    <p class="card-text">Chutes ao gol/jogo: {{ playerDetails.ShotsOnTarget }}</p>
+                                    <p class="card-text">Porcentagem de chutes ao gol: {{
+                                        playerDetails.ShotsOnTarget_Percentage
+                                    }}%</p>
+                                    <p class="card-text">Gols/chute: {{ playerDetails.Goals_Shot }}</p>
+                                    <p class="card-text">Gols/chutes ao gol: {{ playerDetails.Goals_ShotsOnTarget }}</p>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="card-body">
-                                <h5 class="card-title">Outras estatísticas</h5>
-                                <p class="card-text">Gols na liga: {{ playerDetails.Goals_Total }}</p>
-                                <p class="card-text">Gols/Jogo: {{ playerDetails.Goals }}</p>
-                                <p class="card-text">Assistências na liga: {{ playerDetails.Goals_Assist }}</p>
-                                <p class="card-text">Assistências/Jogo: {{ playerDetails.Assists }}</p>
+                            <div class="card border-0">
+                                <div class="card-body">
+                                    <h5 class="card-title">Estatisticas de Defesa</h5>
+                                    <p class="card-text">Bloqueios: {{ playerDetails.Tackled_Block }}</p>
+                                    <p class="card-text">Bloqueios/jogo: {{ playerDetails.Blocks }}</p>
+                                    <p class="card-text">Interceptações: {{ playerDetails.Tackled_Intercept }}</p>
+                                    <p class="card-text">Interceptações/jogo: {{ playerDetails.Interceptations }}</p>
+                                    <p class="card-text">Desarmes: {{ playerDetails.Tackled_Total }}</p>
+                                    <p class="card-text">Desarmes/jogo: {{ playerDetails.Tackles }}</p>
+                                    <p class="card-text">Desarmes e interceptações/jogo: {{
+                                        playerDetails.Tackles_Interceptations }}</p>
+                                    <p class="card-text">Duelos ganhos: {{ playerDetails.Duels_Won }}</p>
+                                    <p class="card-text">Total de duelos: {{ playerDetails.Duels_Total }}</p>
+                                    <p class="card-text">Porcentagem de duelos ganhos: {{
+                                        playerDetails.Duels_Won_Percentage
+                                    }}%
+                                    </p>
+                                    <p class="card-text">Faltas cometidas: {{ playerDetails.Fouls_Committed }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card border-0">
+                                <div class="card-body">
+                                    <h5 class="card-title">Estatísticas de criação</h5>
+                                    <p class="card-text">Assistências na liga: {{ playerDetails.Goals_Assist }}</p>
+                                    <p class="card-text">Assistências/Jogo: {{ playerDetails.Assists }}</p>
+                                    <p class="card-text">Precisão dos passes: {{ playerDetails.Accuracy_Passes }}%</p>
+                                    <p class="card-text">Passes chave: {{ playerDetails.Key_Passes }}</p>
+                                    <p class="card-text">Passes chave/jogo: {{ playerDetails.Key_Passes_n }}</p>
+                                    <p class="card-text">Total de passes: {{ playerDetails.Total_Passes }}</p>
+                                    <p class="card-text">Passes/jogo: {{ playerDetails.Passes_Total_n }}</p>
+                                    <p class="card-text">Dribles tentados: {{ playerDetails.Dribbles_Attempts }}</p>
+                                    <p class="card-text">Dribles completos: {{ playerDetails.Dribbles_Success }}</p>
+                                    <p class="card-text">Porcentagem de dribles completos: {{
+                                        playerDetails.Dribbles_Success_Percentage }}%</p>
+                                    <p class="card-text">Faltas recebidas: {{ playerDetails.Fouls_Drawn }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card border-0">
+                                <div class="card-body">
+                                    <h5 class="card-title">Outras estatísticas</h5>
+                                    <p class="card-text">Cartões amarelos: {{ playerDetails.Yellow_Cards }}</p>
+                                    <p class="card-text">Cartões vermelhos: {{ playerDetails.Red_Cards }}</p>
+                                    <p class="card-text">Dois amarelos: {{ playerDetails.Yellow_Red_Cards }}</p>
+                                    <p class="card-text">Nota: {{ playerDetails.Rating }}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
