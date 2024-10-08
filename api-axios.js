45ec0188af10acf63f9fb12912f63ad75916c664
Pablo Ghidini <pablo.ghidini@youtan.com.br>
2024-10-08 18:33:20 -0300

    package update

--- api-axios.js
+++ api-axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getAPI = axios.create({
     baseURL: "http://127.0.0.1:8000/api/v1/",
-    timeout: 1000,
+    timeout: 2000,
 });
 
 getAPI.interceptors.request.use(
